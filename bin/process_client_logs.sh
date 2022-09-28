c2c9354d5b4a7546f97a43b29d020be635d332ed
Daniel Albright <albri118@morris.umn.edu>
2022-09-28 10:53:49 -0500

    create script for failed_login_data collection of target directory
    takes a name of a log directory cats all log files and pulls out only failed login attempt lines, writing to failed_login_data.txt in the same directory

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-tar_dir=$1
-parent_dir=$(${pwd}/log_files)
+# Store input directory
+target_dir=$1
 
-tar -zxf "${parent_dir}/${tar_dir}" | grep > failed_login_data.txt
+# cat the failed login attempt lines from each file in the target dir and write to new file in same dir
+cat "${target_dir}"/var/log/* | awk 'match($0, /([a-zA-Z]+) *([0-9]{1,2}) *([0-9]{2}):[0-9]{2}:[0-9]{2} [a-zA-Z_0-9\-]+ sshd\[[0-9]+]: Failed password for (invalid user )?([a-zA-Z0-9_\-]+) from ([0-9.]+) port [0-9]+ ssh2/, groups) {print groups[1] " " groups[2] " " groups[3] " " groups[5] " " groups[6]}' > "$target_dir"/failed_login_data.txt
